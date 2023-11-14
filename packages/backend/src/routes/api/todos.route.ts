f7b80fdf359ecc248ef35cae7cd5d592db8e7057
Олексанр Чумак <cauua@air-oleksanr.lan>
2023-11-14 15:38:20 +0100

    feat: Added routes

--- packages/backend/src/routes/api/todos.route.ts
+++ packages/backend/src/routes/api/todos.route.ts
@@ -1,9 +1,37 @@
 import { Router } from 'express';
-
 import todoController from '../../controllers/todo.controller';
+import { createTodoSchema, updateTodoSchema } from '../../schemas/todo';
+import { IsExistMiddleware } from '../../middleware/is-exist.middleware';
+import { TodoEntity } from '../../entities';
 
 const todosRouter: Router = Router();
 
-todosRouter.get('', todoController.getAllTodo.bind(todoController));
+todosRouter.get('/', todoController.index.bind(todoController));
+
+todosRouter.get(
+  '/:id',
+  ...[IsExistMiddleware(TodoEntity, 'Todo not found')],
+  todoController.show.bind(todoController)
+);
+
+todosRouter.post('/', ...createTodoSchema, todoController.store.bind(todoController));
+
+todosRouter.put(
+  '/:id',
+  ...[IsExistMiddleware(TodoEntity, 'Todo not found'), ...updateTodoSchema],
+  todoController.update.bind(todoController)
+);
+
+todosRouter.patch(
+  '/:id',
+  ...[IsExistMiddleware(TodoEntity, 'Todo not found'), ...updateTodoSchema],
+  todoController.update.bind(todoController)
+);
+
+todosRouter.delete(
+  '/:id',
+  ...[IsExistMiddleware(TodoEntity, 'Todo not found')],
+  todoController.destroy.bind(todoController)
+);
 
 export default todosRouter;
