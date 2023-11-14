61d6062971b16007e2ddb710e100929482f29a53
Олексанр Чумак <cauua@air-oleksanr.lan>
2023-11-14 15:40:36 +0100

    feat: Added todo service

--- packages/backend/src/services/todo.service.ts
+++ packages/backend/src/services/todo.service.ts
@@ -1,5 +1,24 @@
+import { DeepPartial, DeleteResult, UpdateResult } from 'typeorm';
+import { TodoEntity } from '../entities';
+
 export default class TodoService {
-  async findAll() {
-    return 'Todos';
+  async index(): Promise<TodoEntity[]> {
+    return TodoEntity.find();
+  }
+
+  async store(data: DeepPartial<TodoEntity>): Promise<TodoEntity> {
+    return TodoEntity.save(data);
+  }
+
+  async show(id: number): Promise<TodoEntity | null> {
+    return TodoEntity.findOne({ where: { id } });
+  }
+
+  async update(criteria: number, data: DeepPartial<TodoEntity>): Promise<UpdateResult> {
+    return TodoEntity.update(criteria, data);
+  }
+
+  async destroy(id: number): Promise<DeleteResult> {
+    return TodoEntity.delete({ id });
   }
 }
