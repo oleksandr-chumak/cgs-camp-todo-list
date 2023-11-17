a5ab221aa562a07bca1b16be8a6f4def471ce0b8
Олексанр Чумак <cauua@air-oleksanr.lan>
2023-11-17 23:04:26 +0100

    feat: Added todo model

---
+++ packages/frontend/src/modules/todo/models/todo.model.ts
@@ -0,0 +1,55 @@
+import {
+  ACCESS,
+  DeserializedTodoWithIdAndTimestamps,
+  STATUS,
+  TodoWithIdAndTimestamps
+} from '../features/types/todos.type';
+
+export class TodoModel implements TodoWithIdAndTimestamps {
+  id: number;
+
+  title: string;
+
+  content: string;
+
+  status: STATUS;
+
+  access: ACCESS;
+
+  createdAt: Date;
+
+  updatedAt: Date;
+
+  constructor(deserializedData: DeserializedTodoWithIdAndTimestamps) {
+    this.id = deserializedData.id;
+    this.title = deserializedData.title;
+    this.content = deserializedData.content;
+    this.status = deserializedData.status;
+    this.access = deserializedData.access;
+    this.createdAt = new Date(deserializedData.createdAt);
+    this.updatedAt = new Date(deserializedData.updatedAt);
+  }
+
+  get formattedDate(): string {
+    const months: string[] = [
+      'Jan',
+      'Feb',
+      'Mar',
+      'Apr',
+      'May',
+      'Jun',
+      'Jul',
+      'Aug',
+      'Sep',
+      'Oct',
+      'Nov',
+      'Dec'
+    ];
+
+    const day: number = this.createdAt.getDate();
+    const monthIndex: number = this.createdAt.getMonth();
+    const year: number = this.createdAt.getFullYear();
+
+    return `${day} ${months[monthIndex]} ${year}`;
+  }
+}
