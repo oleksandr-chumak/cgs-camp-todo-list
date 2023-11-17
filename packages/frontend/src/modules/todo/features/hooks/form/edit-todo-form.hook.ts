3b6a29fb52ad7abbf9023f01e6f49f5ab6936b76
Олексанр Чумак <cauua@air-oleksanr.lan>
2023-11-17 23:03:49 +0100

    feat: Added form hooks

---
+++ packages/frontend/src/modules/todo/features/hooks/form/edit-todo-form.hook.ts
@@ -0,0 +1,74 @@
+import { useEffect } from 'react';
+import { useFormik } from 'formik';
+import toast from 'react-hot-toast';
+import { useGetTodo } from '../query/get-todo.hook';
+import { ACCESS, STATUS, Todo } from '../../types/todos.type';
+import { useUpdateTodo } from '../query/update-todo.hook';
+import { useModalContext } from '../../../../common/hooks/modal-context.hook';
+import { todoSchema } from '../../schemas/todo.schema';
+
+export const useEditTodoForm = (id: number) => {
+  const { data, isLoading } = useGetTodo(id);
+  const { closeModal } = useModalContext();
+
+  const onSuccess = () => {
+    toast.success('Todo updated');
+    closeModal();
+  };
+
+  const { updateTodo } = useUpdateTodo({ fetch: true, onSuccess });
+
+  const handleSubmit = (values: Todo) => {
+    updateTodo({ ...values, id });
+  };
+
+  const formik = useFormik<Todo>({
+    initialValues: { title: '', content: '', access: ACCESS.PUBLIC, status: STATUS.IN_PROGRESS },
+    validationSchema: todoSchema,
+    onSubmit: handleSubmit
+  });
+
+  useEffect(() => {
+    if (data) {
+      formik.setValues({
+        title: data.title,
+        status: data.status,
+        access: data.access,
+        content: data.content
+      });
+    }
+  }, [data]);
+
+  const getFieldProperties = (name: keyof Todo) => ({
+    id: name,
+    name,
+    onChange: formik.handleChange,
+    onBlur: formik.handleBlur,
+    value: formik.values[name]
+  });
+
+  const statusProperties = () => ({
+    value: formik.values.status === STATUS.COMPLETED,
+    onChange: (value: boolean) => {
+      formik.setFieldValue('status', value ? STATUS.COMPLETED : STATUS.IN_PROGRESS);
+    }
+  });
+
+  const accessProperties = () => ({
+    value: formik.values.access === ACCESS.PRIVATE,
+    onChange: (value: boolean) => {
+      formik.setFieldValue('access', value ? ACCESS.PRIVATE : ACCESS.PUBLIC);
+    }
+  });
+
+  const isError = (name: keyof Todo) => formik.touched[name] && formik.errors[name];
+
+  return {
+    isLoading,
+    formik,
+    isError,
+    getFieldProperties,
+    statusProperties,
+    accessProperties
+  };
+};
---
+++ packages/frontend/src/modules/todo/features/schemas/todo.schema.ts
@@ -0,0 +1,12 @@
+import * as Yup from 'yup';
+
+export const todoSchema = Yup.object({
+  title: Yup.string()
+    .required('Title should be')
+    .min(3, 'Tittle should be longer than 3')
+    .max(50, 'Title should be shorter than 3'),
+  content: Yup.string()
+    .required('Title should be')
+    .min(3, 'Content should be longer than 3')
+    .max(250, 'Content should be shorter than 250')
+});
