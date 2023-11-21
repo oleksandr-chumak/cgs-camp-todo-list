768ac817493ccf542ec0f04debea223e0d3d11fb
Олексанр Чумак <cauua@air-oleksanr.lan>
2023-11-21 11:39:00 +0100

    feat: Added confirmation page

---
+++ packages/frontend/src/modules/auth/features/components/ConfirmationPageContent/confirmation-page-content.styled.ts
@@ -0,0 +1,34 @@
+import styled from 'styled-components';
+import { GiConfirmed } from 'react-icons/gi';
+import { COLORS, DISPLAY } from '../../../../theme';
+
+export const ConfirmationContentWrapper = styled('div')`
+  display: flex;
+  gap: 20px;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const ConfirmationContentHeaderWrapper = styled('div')`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+`;
+
+export const ConfirmIcon = styled(GiConfirmed)`
+  color: ${COLORS.green};
+  font-size: 40px;
+  @media (max-width: ${DISPLAY.MOBILE}px) {
+    font-size: 32px;
+  }
+`;
+
+export const ConfirmationContentHeader = styled('h1')`
+  color: ${COLORS.green};
+  font-size: 32px;
+
+  @media (max-width: ${DISPLAY.MOBILE}px) {
+    font-size: 28px;
+  }
+`;
